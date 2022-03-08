47f2d36012c26f6d9e081524816aef2e4042fc28
javut <jaider.vergara@globalbank.com.pa>
2022-03-08 14:52:10 -0500

    commit de prueba

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+
+const apiKey = 'CsNzfGj3VFrdvQIAkkHxrJUGwGJJV05N'
+
+const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+peticion
+    .then(resp => resp.json())
+    .then(({ data }) => {
+        const { url } = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append(img);
+        console.log("Ejemplo")
+        console.log(url);
+    })
+
+
+    .catch(console.warn);
+
