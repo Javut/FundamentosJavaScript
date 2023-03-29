9fa256943c85f92dc70279a83a9fc2079590e694
Javut <javut2010@hotmail.com>
2023-03-29 10:20:06 -0500

    Update 08-imp-ex.js

--- src/bases/08-imp-ex.js
+++ src/bases/08-imp-ex.js
@@ -9,3 +9,6 @@ getHeroeById(1)
 export const getHeroesByOwner = (owner) => heroes.filter( (heroe) => heroe.owner === owner )
 
 // console.log(getHeroesByOwner('DC'))
+
+
+// Las exportaciones por default al momento de importarse no requieren llaven, mientras que el otro tipo de importación si 
