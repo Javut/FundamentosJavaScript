2192a08441202074d0cc2ec0459ba0fd1e40ed7c
Javut <javut2010@hotmail.com>
2023-03-29 10:18:41 -0500

    Update and rename Condicional-Terneario.js to condicional-terneario.js

---
+++ src/bases/condicional-terneario.js
@@ -0,0 +1,13 @@
+
+//Condicional Terneario 
+
+const activo = true;
+
+
+//  const mensaje =  (activo) ? "Se encuentra activo" : "Se encuentra inactivo");
+//  console.log(mensaje);
+
+ const mensaje = activo && 'Activo'; //Forma corta de hacer un if 
+
+ console.log(mensaje);
+
